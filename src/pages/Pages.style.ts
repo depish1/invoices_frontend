f3d7a779269e5cfd563cab5e59a8847646e6337a
depish1 <michal.depa@rsqtechnologies.com>
2023-04-06 00:58:19 +0200

    fix vertical scroll on login and registration pages

--- src/pages/Pages.style.ts
+++ src/pages/Pages.style.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const MainWrapper = styled.main`
   background: ${({ theme }) => theme.colors.background2};
   border-radius: 2rem;
+  max-height: 100%;
   max-width: 100%;
+  overflow-y: scroll;
   padding: 4rem;
 `;
